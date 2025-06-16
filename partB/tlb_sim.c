a4a99a6fe324058e0765e0665621ddd7564a538f
zen1612 <zenkhoo16@gmail.com>
2025-06-16 10:52:37 +0900

    Create tlb_sim.c

---
+++ partB/tlb_sim.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SIZE 1024*1024*512 
+#define PAGE_SIZE 4096 
+
+int main(int argc, char *argv[]) {
+    int *arr = malloc(SIZE);
+    int steps = SIZE / sizeof(int);
+    srand(time(NULL));
+
+    for (int i = 0; i < steps; i += (PAGE_SIZE / sizeof(int))) {
+        int index = (argv[1][0] == 'r') ? (rand() % (steps / (PAGE_SIZE / sizeof(int)))) * (PAGE_SIZE / sizeof(int)) : i;
+        arr[index]++;
+    }
+
+    free(arr);
+    return 0;
+}
