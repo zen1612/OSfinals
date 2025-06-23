b733257d3ae542b165227ccacbeb9945bcd576b0
zen1612 <zenkhoo16@gmail.com>
2025-06-23 14:10:54 +0900

    Update tlb_sim.c

--- partB/tlb_sim.c
+++ partB/tlb_sim.c
@@ -3,12 +3,17 @@
 #include <time.h>
 #include <unistd.h>
 
-#define SIZE (512 * 1024 * 1024)  // 512MB
+#define SIZE (512 * 1024 * 1024)
 #define PAGE_SIZE 4096
+
+double get_time_diff(struct timespec *start, struct timespec *end) {
+    return (end->tv_sec - start->tv_sec) + 
+           (end->tv_nsec - start->tv_nsec) / 1e9;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2 || (argv[1][0] != 'r' && argv[1][0] != 's')) {
-        fprintf(stderr, "Usage: %s [s|r] (s = sequential, r = random)\n", argv[0]);
+        fprintf(stderr, "Usage: %s [s|r]\n", argv[0]);
         return 1;
     }
 
@@ -21,17 +26,26 @@ int main(int argc, char *argv[]) {
 
     int stride = PAGE_SIZE / sizeof(int);
     int pages = steps / stride;
-
     srand(time(NULL));
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     for (int i = 0; i < steps; i += stride) {
         int index = (argv[1][0] == 'r') ? (rand() % pages) * stride : i;
         arr[index]++;
     }
 
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    printf("%s access time: %.6f seconds\n",
+           (argv[1][0] == 'r') ? "Random" : "Sequential",
+           get_time_diff(&start, &end));
+
     free(arr);
     return 0;
 }
+
 
 //gcc -O2 -o tlb_test tlb_test.c
 //perf stat -e dTLB-loads,dTLB-load-misses ./tlb_test s  # sequential
