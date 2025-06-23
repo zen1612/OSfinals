e7107ec39e5c66a32631228970da6b1b25e7afe4
zen1612 <zenkhoo16@gmail.com>
2025-06-23 14:21:41 +0900

    Update cow_sim.c

--- partB/cow_sim.c
+++ partB/cow_sim.c
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
+void show_memory(const char *who) {
+    char cmd[128];
+    snprintf(cmd, sizeof(cmd), "echo '%s (PID %d)' && grep Vm /proc/%d/status", who, getpid(), getpid());
+    system(cmd);
+}
 
 int main() {
     char *mem = malloc(4096);
     strcpy(mem, "test string");
 
+    show_memory("Parent before fork");
+
     pid_t pid = fork();
     if (pid == 0) {
-        printf("Child: %s\n", mem);
-        mem[0] = 'X';
-        printf("Child modifies to: %s\n", mem);
+        show_memory("Child before write");
+
+        mem[0] = 'X';  
+
+        show_memory("Child after write");
+        printf("Child modified: %s\n", mem);
+        _exit(0);
     } else {
         wait(NULL);
-        printf("Parent: %s\n", mem);
+        show_memory("Parent after child");
+        printf("Parent sees: %s\n", mem);
     }
 
     free(mem);
